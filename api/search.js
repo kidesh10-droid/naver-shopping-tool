84a6e2e6783517ca763364d9f00ae559ed3d4ec2
kidesh10-droid <kidesh10@gmail.com>
2026-04-05 08:49:25 +0900

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,17 +1,24 @@
 const https = require('https');
-const url = require('url');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  const { clientId, clientSecret, query, start, display, sort, filter } = req.query;
+  const clientId = process.env.NAVER_CLIENT_ID;
+  const clientSecret = process.env.NAVER_CLIENT_SECRET;
 
-  if (!clientId || !clientSecret || !query) {
-    return res.status(400).json({ errorMessage: '필수 파라미터가 없습니다.' });
+  const { query, start, display, sort, filter } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ errorMessage: '검색어를 입력해주세요.' });
   }
 
-  const params = new URLSearchParams({ query, start: start||'1', display: display||'100', sort: sort||'sim' });
+  const params = new URLSearchParams({
+    query,
+    start: start || '1',
+    display: display || '100',
+    sort: sort || 'sim'
+  });
   if (filter) params.append('filter', filter);
 
   const options = {
@@ -31,7 +38,6 @@ module.exports = async (req, res) => {
         r.on('end', () => resolve({ status: r.statusCode, body }));
       }).on('error', reject);
     });
-
     res.status(data.status).send(data.body);
   } catch (e) {
     res.status(500).json({ errorMessage: e.message });
